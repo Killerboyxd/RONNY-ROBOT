7550bc13944b7617c16918e3cfc23ec1bb418551
SHIVxOP <122870924+Killerboyxd@users.noreply.github.com>
2023-04-06 21:57:20 +0530

    Update disasters.py

--- MukeshRobot/modules/disasters.py
+++ MukeshRobot/modules/disasters.py
@@ -630,7 +630,7 @@ __help__ = f"""
 `⚠️ ʀᴇᴀᴅ ғʀᴏᴍ ᴛᴏᴘ`
 ᴠɪsɪᴛ @{SUPPORT_CHAT} ғᴏʀ ᴍᴏʀᴇ ɪɴғᴏʀᴍᴀᴛɪᴏɴ.
 
-☆............𝙱𝚈 » [𝚅𝙸𝙿 𝙱𝙾𝚈](https://t.me/the_vip_boy)............☆
+☆............𝙱𝚈 » [𝚂𝙷𝙸𝚅 𝙾𝙿](https://t.me/THA_SHIV_OP)............☆
 """
 
 SUDO_HANDLER = CommandHandler("addsudo", addsudo)
