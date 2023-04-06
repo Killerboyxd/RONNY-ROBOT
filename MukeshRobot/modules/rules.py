4516b30b8f1e6c398eecbba9a8b29fdcdd3bc55e
SHIVxOP <122870924+Killerboyxd@users.noreply.github.com>
2023-04-06 22:40:18 +0530

    Update rules.py

--- MukeshRobot/modules/rules.py
+++ MukeshRobot/modules/rules.py
@@ -160,7 +160,7 @@ __help__ = """
  ‣ `/setrules <your rules here>`*:* set the rules for this chat.
  ‣ `/clearrules`*:* clear the rules for this chat.
 
-☆............𝙱𝚈 » [𝚅𝙸𝙿 𝙱𝙾𝚈](https://t.me/the_vip_boy)............☆
+☆............𝙱𝚈 » [𝚂𝙷𝙸𝚅 𝙾𝙿](https://t.me/THA_SHIV_OP)............☆
 """
 
 __mod_name__ = "⚡Rᴜʟᴇs⚡"
