e62f3d1251f1904c57551810fdd96d9cadf743f6
SHIVxOP <122870924+Killerboyxd@users.noreply.github.com>
2023-04-06 22:39:02 +0530

    Update reporting.py

--- MukeshRobot/modules/reporting.py
+++ MukeshRobot/modules/reporting.py
@@ -274,7 +274,7 @@ __help__ = """
    • ɪғ ᴅᴏɴᴇ ɪɴ ᴘᴍ, ᴛᴏɢɢʟᴇs ʏᴏᴜʀ sᴛᴀᴛᴜs.
    • ɪғ ɪɴ ɢʀᴏᴜᴘ, ᴛᴏɢɢʟᴇs ᴛʜᴀᴛ ɢʀᴏᴜᴘs's sᴛᴀᴛᴜs.
 
-☆............𝙱𝚈 » [𝚅𝙸𝙿 𝙱𝙾𝚈](https://t.me/the_vip_boy)............☆
+☆............𝙱𝚈 » [𝚂𝙷𝙸𝚅 𝙾𝙿](https://t.me/THA_SHIV_OP)............☆
 """
 
 SETTING_HANDLER = CommandHandler("reports", report_setting)
